a6d49423ea2195f07e8007858650ddb41a76ba8b
nebevets <steveben@exmsft.com>
2019-04-04 10:20:47 -0700

    react-intro - steve benedict

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,8 @@
+import React from 'react';
+import Greeting from './greeting';
+import userObject from './userObject';
 
+const App = () =>
+  <Greeting name={userObject.name} luckyNumber={userObject.luckyNumber} />;
 
+export default App;
---
+++ src/components/userObject.js
@@ -0,0 +1,6 @@
+const userObject = {
+  name: 'Steve',
+  luckyNumber: () => Math.floor((Math.random() * 1000) + 1),
+}
+
+export default userObject;
