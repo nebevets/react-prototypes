4096847ba40098ace19424e4dc3f6562d9d4cb15
nebevets <steveben@exmsft.com>
2019-04-18 10:24:30 -0700

    feature set 2 complete

--- src/components/movie_list.js
+++ src/components/movie_list.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import Movie from './movie';
+import axios from 'axios';
 
 class MovieList extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      movies: []
+    }
+  }
+  componentDidMount(){
+    const iTunesMovieURL = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
+    axios
+      .get(iTunesMovieURL)
+      .then((response) => {
+        const [movies] = response.data.feed.entry;
+        this.setState({
+          movies
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
   render(){
+    console.log(this.state);
     return(
       <div> 
         <h2>Top Ten Movies</h2>
